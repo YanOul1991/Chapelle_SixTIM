ef50a5902e085263dfd2e725c84c94cb377c5243
Emile Litalien <e2269456@cmaisonneuve.qc.ca>
2025-10-29 13:36:59 -0400

    Revamp footer, front page, and single post layout
    Major updates to footer.php with dynamic social links, navigation, and legal sections. front-page.php now displays parent and child project categories more clearly. single-post.php enhanced to show project details and members using custom fields. Added normalize.css for cross-browser consistency and updated style.css for new footer and single post styles. Minor improvements to creators page and header.

--- footer.php
+++ footer.php
@@ -1,4 +1,61 @@
-<footer style="position: relative;">
+    <?php $icone_1_img = get_theme_mod("social_1_icon", "http://localhost:81/4w4/wp-content/uploads/2025/02/25231.png") ?>
+    <?php $icone_1_url = get_theme_mod("social_1_url", "Default") ?>
+    <?php $icone_1_name = get_theme_mod("social_1_name", "Default") ?>
+    <?php $icone_2_img = get_theme_mod("social_2_icon", "http://localhost:81/4w4/wp-content/uploads/2025/02/77364-instagram-icons-computer-black-logo-white-wine_600x600.png") ?>
+    <?php $icone_2_url = get_theme_mod("social_2_url", "Default") ?>
+    <?php $icone_2_name = get_theme_mod("social_2_name", "Default") ?>
+    <?php $icone_3_img = get_theme_mod("social_3_icon", "FacebookIconBlack.png") ?>
+    <?php $icone_3_url = get_theme_mod("social_3_url", "Default") ?>
+    <?php $icone_3_name = get_theme_mod("social_3_name", "Default") ?>
+    <?php $icone_4_img = get_theme_mod("social_4_icon", "5a2fe479cc45e43754640849.png") ?>
+    <?php $icone_4_url = get_theme_mod("social_4_url", "Default") ?>
+    <?php $icone_4_name = get_theme_mod("social_4_name", "Default") ?>
+
+
+
+
+<div id="footer_complet">
   <?php get_caller(); ?>
-  <h3>This is the footer :></h3>
-</footer>
+
+    <div id="footer_navs"> <!-- Navs -->
+        <h3>Les pages du site</h3>
+        <?php get_nav('footer'); ?>
+  </div>
+
+  <div id="footer_central"> <!-- Section centrale (logo, réseaux, légal) -->
+
+    <div id="footer_logo_contenant"> <!-- L'image logo -->
+      <div class="footer_logo">
+        <?php the_custom_logo(); ?>
+      </div>
+    </div>
+    
+    <div id="footer_social_contenant"> <!-- Les liens vers réseaux sociaux | Customizer -->
+      <div class="social-links">
+        <a href="<?php echo $icone_1_url ?>">
+          <img class="social-links-icon" src="<?php echo $icone_1_img ?>" alt="<?php echo $icone_1_name ?>" width="32" height="32">
+        </a>
+        <a href="<?php echo $icone_2_url ?>">
+          <img class="social-links-icon" src="<?php echo $icone_2_img ?>" alt="<?php echo $icone_2_name ?>" width="32" height="32">
+        </a>
+        <a href="<?php echo $icone_3_url ?>">
+          <img class="social-links-icon" src="<?php echo $icone_3_img ?>" alt="<?php echo $icone_3_name ?>" width="32" height="32">
+        </a>
+        <a href="<?php echo $icone_4_url ?>">
+          <img class="social-links-icon" src="<?php echo $icone_4_img ?>" alt="<?php echo $icone_4_name ?>" width="32" height="32">
+        </a>
+      </div>
+    </div>
+    
+    <div id="footer_legal_contenant"> <!-- Mention légal -->
+      <h3>Mentions légales</h3>
+      <p>© ChapelleSixTIM - Tous droits réservés.</p> <!-- À revoir pour rendre dynamique genre un champ customizer -->
+    </div>
+  </div>
+
+  <div id="footer_a_propos"> <!-- À propos --> 
+    <h3>À propos de l'exposition</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium tempora optio, vel, mollitia impedit dolore ratione ut nobis nisi quas quibusdam? Eveniet perspiciatis tempora fugit ab veniam officiis, tenetur labore.</p>
+  </div>
+
+</div>
--- single-post.php
+++ single-post.php
@@ -1,9 +1,24 @@
 <?php get_header(); ?>
-<main>
+<main class="single-post-content">
   <?php get_caller(); ?>
-  <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-    <h1><?php the_title();?></h1>
-    <?php the_content() ?>
-  <?php endwhile; endif;?>
+  <?php if (have_posts()) : the_post(); ?>
+    <h1 class="single-project-titre single-project-texte-item"><?php print_r(get_field('projet_nom')); ?></h1>
+    <h1 class="single-project-type single-project-texte-item"><?php print_r(get_field('projet_cours')['label']); ?></h1>
+    <img class="single-project-thumbnail" src="https://placehold.co/500x500/FF9900/FFFFFF" alt="Projet Thumbnail">
+    
+    <?php
+    $query = new WP_Query([
+      'post__in' =>   get_field('projet_eleves'),
+      'post_type' => 'any',
+      'order_by'  => 'post_in'
+    ]); ?> 
+    <div class="single-project-membres single-project-texte-item">
+      <?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post() ?>
+          <h3 class="single-project-membres-item"><?php echo (get_field('eleve_nom') . " " . get_field('eleve_prenom') . " - " . get_field('eleve_contribution')) ?></h3>
+      <?php endwhile; endif; wp_reset_postdata(); ?>
+    </div>
+    <p class="single-project-description single-project-texte-item"><?php print_r(get_field('projet_description')) ?></p>
+  <?php endif; ?>
 </main>
-<?php wp_footer(); get_footer(); ?>
+<?php wp_footer();
+get_footer(); ?>
