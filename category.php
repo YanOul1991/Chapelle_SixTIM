f58cd834fec906140e56f8d887dfb4c98f6cc964
Bah <e2233003@cmaisonneuve.qc.ca>
2025-11-13 15:10:31 -0500

    jen ai marre

--- category.php
+++ category.php
@@ -1,6 +1,7 @@
 <?php get_header() ?>
 <main>
   <?php get_caller() ?>
+  <h2 class="project-gallery-title">Tout les projets etudiants.</h2>
   <div class="project-gallery">
     <ul class="project-gallery-list">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
