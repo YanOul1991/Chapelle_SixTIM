1d80a3607351d4e9c52096dee3b73e7f307f38ac
Yan <e2073117@cmaisonneuve.qc.ca>
2025-11-13 16:05:56 -0500

    Mini changements.

--- category.php
+++ category.php
@@ -4,7 +4,7 @@
   <div class="project-gallery">
     <ul class="project-gallery-list">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          <li class="project-gallery-list-item <?php echo "projet".get_the_category()[0]->slug ?>">
+          <li class="project-gallery-list-item <?php echo "projet-".get_the_category()[0]->slug ?>">
             <?php get_project_card("project-gallery-list-item"); ?>
           </li>
       <?php endwhile;
