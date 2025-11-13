f853421a94a42726c0cc06e12adb53da101df0ff
Yan <e2073117@cmaisonneuve.qc.ca>
2025-11-13 14:36:15 -0500

    Ajout CSS pour la page d'accueil et la page des créateurs.

--- page-creators.php
+++ page-creators.php
@@ -1,17 +1,19 @@
 <?php get_header() ?>
 
-<main>
+<main class="createurs">
   <?php get_caller()?>
-  <h1>createurs</h1>
+  <h1 class="titre-page">Createurs</h1>
   <ul class="liste-createurs">
     <?php for($i = 0; $i < 4; $i++) : ?>
     <li class="liste-createurs-item">
-      <div class="createur-nom">Createur #<?php echo ($i + 1); ?></div>
-      <div class="createur-image" style="width: 250px;">
-        <img src="https://placehold.co/500x500/FF9900/FFFFFF" alt="Image Createur" style="width: 100%;">
+      <div class="createur-texte">
+        <div class="createur-nom">Createur #<?php echo ($i + 1); ?></div>
+        <div class="createur-description">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam harum eveniet voluptates repellendus perferendis facere asperiores tenetur iusto facilis in.
+        </div>
       </div>
-      <div class="createur-description">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam harum eveniet voluptates repellendus perferendis facere asperiores tenetur iusto facilis in.
+      <div class="createur-image">
+        <img src="https://placehold.co/500x500/FF9900/FFFFFF" alt="Image Createur" style="width: 100%;">
       </div>
     </li>
     <?php endfor; ?>
