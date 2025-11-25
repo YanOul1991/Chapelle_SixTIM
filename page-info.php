5e059c7c86bdf5b062887288702ce9c405b3d06d
Bah <e2233003@cmaisonneuve.qc.ca>
2025-11-25 16:41:30 -0500

    ajustement des couleurs

--- page-info.php
+++ page-info.php
@@ -7,6 +7,7 @@
   <section id="PageInfo">
   <!-- Partie paragraphe d'explications -->
     <section id="ExplicationsContenant">
+      <h2 class="ExplicationTitre">ExpoTIM</h2>
       <p id="ExplicationsTexte">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
