c9cbeeba8f66a554b40bbfca357d4fa696be4c54
N0uh0u <Nouhoubah05@gmail.com>
2025-12-06 12:36:27 -0500

    finition page créateurs

--- page-creators.php
+++ page-creators.php
@@ -2,12 +2,12 @@
 
 <main class="createurs">
   <?php get_caller()?>
-  <h1 class="titre-page">Createurs</h1>
+  <h1 class="titre-page">Créateurs</h1>
   <ul class="liste-createurs">
     <?php for($i = 0; $i < 4; $i++) : ?>
     <li class="liste-createurs-item">
       <div class="createur-texte">
-        <div class="createur-nom">Createur #<?php echo ($i + 1); ?></div>
+        <div class="createur-nom">Créateur #<?php echo ($i + 1); ?></div>
         <div class="createur-description">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam harum eveniet voluptates repellendus perferendis facere asperiores tenetur iusto facilis in.
         </div>
